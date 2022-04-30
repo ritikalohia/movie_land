8f2e21240c32acbc39f94d9d73913ea78bfdc3ec
Ritika lohia <56797604+ritikalohia@users.noreply.github.com>
2022-04-30 07:23:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 //b2516965
 
-const API_URL ="http://www.omdbapi.com?apikey=b2516965";
+const API_URL ="http://www.omdbapi.com?apikey=e94b6729";
 
 const App = () => {
     const [movies, setMovies] = useState([]);
